d1e21eca0312361fe970ef1f8249dfe6aa7615b8
liupingyin <liupingyin@xjgreat.com>
2016-10-14 17:40:50 +0800

    新建

---
+++ js/index.js
@@ -0,0 +1,43 @@
+
+var s=window.setInterval(function () {
+  var d=new Date();
+  var y=d.getFullYear();
+  var m=(d.getMonth()+1);
+  var date=d.getDate();
+  var h=d.getHours();
+  var min=d.getMinutes();
+  var s=d.getSeconds();
+
+  $('.day').html(y+'-'+addZero(m)+'-'+addZero(date));
+  $('.time').html(addZero(h)+':'+addZero(min)+':'+addZero(s));
+},1000);
+
+
+function addZero(num) {
+  if(num<10){
+    num='0'+num;
+  }
+  return num;
+
+}
+$('.show-btn').on('click',function () {
+  if($(this).hasClass('show')){
+    $('#content').animate({'margin-left':'0'},1000);
+    $('#left_nav').animate({left:'-165px'},1000);
+    $(this).find('.show-span').html('>');
+  }else{
+    $('#content').animate({'margin-left':'165px'},1000);
+    $('#left_nav').animate({left:'0'},1000);
+    $(this).find('.show-span').html('<');
+  }
+  $(this).toggleClass('show')
+});
+
+$(".xiaoqu-btn").on('click',function () {
+  $(this).siblings().removeClass('active');
+  $(this).addClass('active');
+});
+$("#left_nav .list").on('click',function () {
+  $(this).siblings().removeClass('active');
+  $(this).addClass('active');
+})
